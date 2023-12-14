62c5751f074678fe1001ad6f663604387c4c6a75
Wojciech Walczuk <s95600@pollub.edu.pl>
2023-12-14 01:29:04 +0100

    Create class Review,Movie,CLient

---
+++ src/main/java/com/example/demo/domain/Client.java
@@ -0,0 +1,22 @@
+package com.example.demo.domain;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity @Getter @Setter @NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @OneToMany(mappedBy = "client")
+    private List<Review> reviews;
+}
